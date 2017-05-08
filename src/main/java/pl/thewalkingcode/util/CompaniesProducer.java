ceb7a67b85cfe782a35e9d995b296b95863e8671
Marek Chrapek <mchrapek@omnis.pl>
2017-05-08 21:53:30 +0200

    more companies

--- src/main/java/pl/thewalkingcode/util/CompaniesProducer.java
+++ src/main/java/pl/thewalkingcode/util/CompaniesProducer.java
@@ -13,6 +13,15 @@ public class CompaniesProducer {
         companies.add(new Company.CompanyBuilder().fullName("Facebook").code("FB").price(new BigDecimal(5).setScale(2, BigDecimal.ROUND_DOWN)).changeToUp(0).build());
         companies.add(new Company.CompanyBuilder().fullName("Ebay").code("EB").price(new BigDecimal(5).setScale(2, BigDecimal.ROUND_DOWN)).changeToUp(0).build());
         companies.add(new Company.CompanyBuilder().fullName("Yahoo").code("YH").price(new BigDecimal(5).setScale(2, BigDecimal.ROUND_DOWN)).changeToUp(0).build());
+        companies.add(new Company.CompanyBuilder().fullName("Apple").code("APL").price(new BigDecimal(4).setScale(2, BigDecimal.ROUND_DOWN)).changeToUp(0).build());
+        companies.add(new Company.CompanyBuilder().fullName("Samsung").code("SAM").price(new BigDecimal(4).setScale(2, BigDecimal.ROUND_DOWN)).changeToUp(0).build());
+        companies.add(new Company.CompanyBuilder().fullName("Amazon").code("AMA").price(new BigDecimal(6).setScale(2, BigDecimal.ROUND_DOWN)).changeToUp(0).build());
+        companies.add(new Company.CompanyBuilder().fullName("Alphabet").code("ALP").price(new BigDecimal(6).setScale(2, BigDecimal.ROUND_DOWN)).changeToUp(0).build());
+        companies.add(new Company.CompanyBuilder().fullName("Micorsoft").code("MS").price(new BigDecimal(4).setScale(2, BigDecimal.ROUND_DOWN)).changeToUp(0).build());
+        companies.add(new Company.CompanyBuilder().fullName("IBM").code("IBM").price(new BigDecimal(4).setScale(2, BigDecimal.ROUND_DOWN)).changeToUp(0).build());
+        companies.add(new Company.CompanyBuilder().fullName("Sony").code("SY").price(new BigDecimal(3).setScale(2, BigDecimal.ROUND_DOWN)).changeToUp(0).build());
+        companies.add(new Company.CompanyBuilder().fullName("Intel").code("INT").price(new BigDecimal(6).setScale(2, BigDecimal.ROUND_DOWN)).changeToUp(0).build());
+
         return companies;
     }
 
